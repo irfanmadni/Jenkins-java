cad3c47a99b4e30898264d9376bb96ba8eeee001
Irfan <46819690+irfanmadni@users.noreply.github.com>
2025-09-20 16:36:36 +0500

    Create Main.java
    Main file created

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+   public static void main(String[] args) {
+      System.out.println("Hello Java Jenkins");
+      System.out.println("This is java jankins test");
+      System.out.println(new Date());
+   }
+}
